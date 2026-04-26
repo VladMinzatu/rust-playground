a4c61841ac935d62d1a0f714e67910133b2c80cd
Vlad Minzatu <minzatu.vlad@gmail.com>
2026-04-26 12:56:24 +0200

    more borrowing

--- examples/references_and_borrowing.rs
+++ examples/references_and_borrowing.rs
@@ -10,6 +10,8 @@ fn main() {
     pointers_and_dereferencing();
 
     permissions();
+
+    borrow_check();
 }
 
 fn sub(first: &String, second: &String) {
@@ -41,4 +43,24 @@ fn permissions() {
 fn return_ref_to_stack() -> String { // could not return &String here (reference to stack value)
     let s = String::from("Hello world");
     s // s is returned by value, so it is moved to the caller, i.e. ownership is mvoed outside of the function
+}
+
+fn borrow_check() {
+    let mut s = String::from("Hello world");
+    take_ownership_BAD(s); // s is moved into the function, so we can no longer use it after this point
+    // println!("s is {s}"); // error: value borrowed here after move
+
+    let mut s2 = String::from("Hello world");
+    borrow_GOOD(&mut s2); // we can still use s2 after this point, since we only borrowed it
+    println!("s2 is {s2}");
+}
+
+fn take_ownership_BAD(mut s: String) { // this makes the parameter s own the String value, so it can modify it, but it also means that the caller can no longer use the value after calling this function - Annoying!
+    s.push_str("!");
+    println!("s is {s}");
+}
+
+fn borrow_GOOD(s: &mut String) { // this borrows the String value, so it can modify it, but the caller can still use the value after calling this function - Much better!
+    s.push_str("!");
+    println!("s is {s}");
 }
