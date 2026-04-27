c90aa143355a845f0742ccc0ccf2eb2c0d90d447
Vlad Minzatu <minzatu.vlad@gmail.com>
2026-04-27 18:54:13 +0200

    mut

--- examples/structs.rs
+++ examples/structs.rs
@@ -8,4 +8,11 @@ fn main() {
         age: 30,
     };
     println!("User1 is {} and is {} years old", user1.name, user1.age);
+
+    let mut user2 = User { // the whole struct must be mutable to modify its fields
+        name: String::from("Bob"),
+        age: 25,
+    };
+    user2.age += 1;
+    println!("User2 is {} and is {} years old", user2.name, user2.age);
 }
