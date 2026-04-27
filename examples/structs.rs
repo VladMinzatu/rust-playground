cff980d6cd3bf828c46514adc4cba1574a465e62
Vlad Minzatu <minzatu.vlad@gmail.com>
2026-04-27 18:55:29 +0200

    field init

--- examples/structs.rs
+++ examples/structs.rs
@@ -9,9 +9,10 @@ fn main() {
     };
     println!("User1 is {} and is {} years old", user1.name, user1.age);
 
+    let age = 26;
     let mut user2 = User { // the whole struct must be mutable to modify its fields
         name: String::from("Bob"),
-        age: 25,
+        age, // field init shorthand, equivalent to age: age
     };
     user2.age += 1;
     println!("User2 is {} and is {} years old", user2.name, user2.age);
