ccb9cde511c5f091085abb7c719aa26b015569cf
Vlad Minzatu <minzatu.vlad@gmail.com>
2026-04-28 18:53:59 +0200

    tuple structs

--- examples/structs.rs
+++ examples/structs.rs
@@ -2,6 +2,9 @@ struct User {
     name: String,
     age: u32,
 }
+
+struct Point(f64, f64); // tuple struct
+
 fn main() {
     let user1 = User {
         name: String::from("Alice"),
@@ -16,4 +19,9 @@ fn main() {
     };
     user2.age += 1;
     println!("User2 is {} and is {} years old", user2.name, user2.age);
+
+    let point = Point(3.0, 4.0);
+    println!("Point is at ({}, {})", point.0, point.1);
+    let Point(x, y) = point; // destructuring the tuple struct
+    println!("(x, y) is at ({}, {})", x, y);
 }
